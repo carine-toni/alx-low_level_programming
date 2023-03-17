f5b554d2786108b7e4916ca19c81d0def568c904
carine-toni <umutoni33@gmail.com>
2023-03-17 16:12:03 -0700

    capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+char t;
+for (t = 'a'; t <= 'z'; ++t)
+{
+putchar(t);
+}
+for (t = 'A'; t <= 'Z'; ++t)
+{
+putchar(t);
+}
+putchar('\n');
+return (0);
+}
