e36e74d4fcfe70860fb91b84191e44307686dc33
carine-toni <umutoni33@gmail.com>
2023-03-19 05:51:54 -0700

    level 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return = 0 (success)
+ */
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '8'; i++)
+for (j = i + '1'; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i != '8' || j != '9')
+{
+{
+putchar(',');
+putchar (' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
