66ac580506586d7686f36a0a8d2d196acefef667
carine-toni <umutoni33@gmail.com>
2023-03-23 11:26:53 -0700

    abc my song

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include <unistd.h>
-
 /**
  * print_alphabet - Prints the alphabet in lowercase
  */
 void print_alphabet(void)
 {
-    char letter = 'a';
-    while (letter <= 'z')
-    {
-        write(1, &letter, 1);
-        letter++;
-    }
-    write(1, "\n", 1);
+char letter = 'a';
+while (letter <= 'z')
+{
+write(1, &letter, 1);
+letter++;
 }
-
+write(1, "\n", 1);
+}
