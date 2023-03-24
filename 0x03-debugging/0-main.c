85715f7b1cf2db701b462c4bca66c0fdfce7b750
carine-toni <umutoni33@gmail.com>
2023-03-24 05:32:46 -0700

    buggs

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,19 +8,8 @@ void positive_or_negative(int i)
 {
 if (i > 0)
 printf("%d is positive\n", i);
-else if (i == 0)
-printf("%d is zero\n", i);
-else
+else if (i < 0)
 printf("%d is negative\n", i);
+else
+printf("%d is zero\n", i);
 }
-/**
- * main - tests the positive_or_negative function
- * Return: 0
- */
-int main(void)
-{
-int i = 0;
-positive_or_negative(i);
-return (0);
-}
-
