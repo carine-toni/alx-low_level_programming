74094067592ff417abcebc0ecbac57045d1956f3
carine-toni <umutoni33@gmail.com>
2023-03-29 07:46:40 -0700

    falsies

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/* Main - Entry Point */
 /**
  * positive_or_negative - prints if integer is positive, negative or zero
  * @i: the integer to be tested
  */
 void positive_or_negative(int i)
 {
-if (i > 0)
-printf("%d is positive\n", i);
-else if (i < 0)
-printf("%d is negative\n", i);
-else
-printf("%d is zero\n", i);
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
 }
