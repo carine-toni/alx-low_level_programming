56ad3af04a7d5b9b6e2d6107f1d7a0cdf4510109
carine-toni <umutoni33@gmail.com>
2023-03-29 14:42:54 -0700

    buggs

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive_or_negative
 * Return: 0
 */
 /* positive_or_negative(i) - function to test integer*/
